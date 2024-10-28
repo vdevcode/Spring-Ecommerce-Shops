47f588261cd6bbc54f5870f69d188cb47a6aece7
Mai Đào Thanh Vũ <108065608+vdevcode@users.noreply.github.com>
2024-10-28 13:12:19 +0700

    chore(init): base source spring marven java

---
+++ src/main/java/com/vdevcode/ecommerce_shops/EcommerceShopsApplication.java
@@ -0,0 +1,13 @@
+package com.vdevcode.ecommerce_shops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceShopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceShopsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vdevcode/ecommerce_shops/EcommerceShopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vdevcode.ecommerce_shops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceShopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
